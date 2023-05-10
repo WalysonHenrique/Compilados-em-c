05c0af854b62c945bbaa87bcb1350e1e7741452c
WalysonHenrique <132800315+WalysonHenrique@users.noreply.github.com>
2023-05-10 15:30:23 -0300

    Update Exe-19.c

--- Exe-19.c
+++ Exe-19.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 int main(void)
 {
-    int numDec, numBin, i;
+    int numDec, numBin, i = 0, tamanhaArray;
     int *binario = NULL;
     printf("Digite um numero na base decimal : \n");
     scanf("%d", &numDec);
     binario = malloc(numDec * sizeof(int));
-    for (i = 0; i > 1; i--)
-    {
+    while (numDec > (1 << tamanhaArray))
+      {
+        tamanhaArray++;
+      } 
+    binario = malloc(tamanhaArray * sizeof(int));    
+    while (numDec >= 1)
+      {
         numBin = numDec % 2;
         numDec = numDec / 2;
         binario[i] = numBin;
-    }
-
-
-
+        i++;
+      }
+    
     return 0;
 }
