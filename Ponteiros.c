feb7adc844c704a834036c9455efeae94ef92d31
Walyson Henrique <walysonhenriqueozoriosobrinho@gmail.com>
2023-11-29 11:26:52 -0300

    att

--- Ponteiros.c
+++ Ponteiros.c
@@ -14,3 +14,26 @@ printf("Variavel apontando para o endereco usando &vetor[0] : %x\nVariavel pveto
     return 0;
 }
 
+/*#include <stdio.h>
+
+// Função para trocar os valores de duas variáveis usando ponteiros
+void trocarValores(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 5;
+    int y = 10;
+
+    printf("Antes da troca: x = %d, y = %d\n", x, y);
+
+    // Chamando a função para trocar os valores usando ponteiros
+    trocarValores(&x, &y);
+
+    printf("Depois da troca: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
+*/
